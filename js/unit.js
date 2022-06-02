65cff35a73a37ced9f43650f20d12506b70dd248
Flamel <flamelmamel@gmail.com>
2022-06-02 15:24:06 +0600

    Changes

---
+++ js/unit.js
@@ -0,0 +1,29 @@
+let cart = {
+    'el1':0
+}
+
+document.onclick = event => {
+    if (event.target.classList.contains('plus')){
+        plusFunction(event.target.dataset.id);
+    }
+    if (event.target.classList.contains('minus')){
+        minusFunction(event.target.dataset.id);
+    }
+}
+
+const minusFunction = id => {
+    if (cart[id]-1 == 0){
+        delete cart[id];
+    }
+    cart[id]--;
+    renderCart();
+}
+
+const plusFunction = id => {
+    cart[id]++;
+    renderCart();
+}
+
+const renderCart = () => {
+    console.log(cart);
+}
